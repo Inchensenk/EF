fbb86cb5786cc5a53e4c86546c824844fe848ccb
Inchensenk <aab8117@gmail.com>
2022-10-14 17:20:46 +0300

    Добавьте файлы проекта.

---
+++ EF/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreStart
+{
+    /// <summary>
+    /// Модель представления
+    /// </summary>
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //using Prism.Commands;
+        public DelegateCommand ClickCommand { get; }
+
+        
+
+        public MainWindowViewModel()
+        {
+            ClickCommand = new DelegateCommand(OnClick);
+        }
+
+        //DbContext реализует интерфейс IDisposable(Предоставляет механизм для освобождения неуправляемых ресурсов.)
+        private void OnClick()
+        {
+            using (MyDbContext context = new MyDbContext())
+            {
+
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+/*
+https://learn.microsoft.com/ru-ru/dotnet/api/system.runtime.compilerservices.callermembernameattribute?view=net-6.0
+https://habr.com/ru/post/201396/
+[CallerMemberName]
+Позволяет получить имя свойства или метода вызывающего метод объекта.
+ 
+DbContext реализует интерфейс IDisposable(Предоставляет механизм для освобождения неуправляемых ресурсов.)
+ */
---
+++ EF/MyDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace EFCoreStart
+{
+    public class MyDbContext : DbContext
+    {
+
+        /// <summary>
+        /// Создаем таблицу пользователей
+        /// 
+        /// DbSet 
+        /// Описывает набор сущностных классов, который затем можно использовать в коде для создания запросов CRUD(create, read, update, delete) к данным.
+        /// С помощью экземпляров этого класса описываются различные объекты базы данных (таблицы, представления, хранимые процедуры и т.д.)
+        /// </summary>
+        public DbSet<User> Users { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=s-dev-01;Database=EntityExplanationDb;Trusted_Connection=True; Encrypt=false");
+        }
+
+    }
+}
+/*
+DbContext
+Является базовым классом Entity Framework и предоставляет широкие возможности по работе с базой данных: 
+создание запросов, отслеживание изменений и сохранение данных в базе.
+
+DbSet
+Описывает набор сущностных классов, который затем можно использовать в коде для создания запросов CRUD (create, read, update, delete) к данным. 
+С помощью экземпляров этого класса описываются различные объекты базы данных (таблицы, представления, хранимые процедуры и т.д.)
+
+
+https://metanit.com/sharp/efcore/2.16.php 
+
+Метод Database.EnsureCreated() и его асинхронная версия Database.EnsureCreatedAsync():
+гарантируют, что база данных будет создана.
+
+Метод Database.EnsureDeleted() и его асинхронная версия Database.EnsureDeletedAsync():
+гарантируют, что база данных будет удалена.
+
+Database.CanConnect
+Еще один метод, который стоит отметить, это Database.CanConnect() и его асинхронная версия Database.CanConnectAsync(). 
+Данный метод возвращает true, если бд доступна, и false, если бд не доступна:
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    bool isAvalaible = db.Database.CanConnect();
+    // bool isAvalaible2 = await db.Database.CanConnectAsync();
+    if (isAvalaible) Console.WriteLine("База данных доступна");
+    else Console.WriteLine("База данных не доступна");
+}
+
+
+
+ */
