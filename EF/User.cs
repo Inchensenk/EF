f24d744fb96d7ba8905baa1d46338da585f6a7ee
Inchensenk <aab8117@gmail.com>
2022-10-18 19:13:10 +0300

    Накатываем обратно миграцию: 20221018154116_AddPhoneNumberColumn
    Снова добавляем колонку phoneNumber
    
    разкоментили phoneNumbeк
    
    команда: Update-Database
    
    Если комманда введена без названия миграции, то накатываются все созданные миграции.
    
    Если ввести определенное имя миграции
    то накатятся либо откатятся миграции до определенной миграции

--- EF/User.cs
+++ EF/User.cs
@@ -12,6 +12,6 @@ namespace EFCoreStart
         public string Name { get; set; }
         public int Age { get; set; }
 
-        //public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
